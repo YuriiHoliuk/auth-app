9da555f4016eebf8744395728a66f5dabe06a355
Olexander <kravchenko.sania@gmail.com>
2020-04-08 11:16:59 +0300

    first

--- server.js
+++ server.js
@@ -10,5 +10,10 @@ app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
+app.patch('/comments/id', (req, res) => {
+  res.send('patch:comments/id')
+})
+
+
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server listening on port ${port}`));

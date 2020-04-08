e1530c8d8d1690f3ad1c3947fa2f218527703647
dima <tynindima@gmail.com>
2020-04-08 11:36:42 +0300

    Added new users

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ const express = require('express');
 const port = 3000;
 
 const app = express();
+const users = [
+  {
+    id: 1, name: 'Dima', age: 31,
+  },
+  {
+    id: 2, name: 'Dave', age: 31,
+  },
+];
 
 app.use(express.json());
 
 app.post('/sign-up', (req, res) => {
-  res.send('Got a POST request');
+  res.json(users);
 });
 
 // eslint-disable-next-line no-console

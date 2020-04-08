29ea5058192dc7fd588f4ecacb39f9d294942594
Yurii Holiuk <bionicuss@gmail.com>
2020-04-08 10:41:29 +0300

    add nodemon and GET /ping endpoint

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const express = require('express');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Server listening on port ${port}`));

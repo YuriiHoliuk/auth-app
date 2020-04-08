4fe00577de0dbeda4f65aaf8e0137982d877768c
dima <tynindima@gmail.com>
2020-04-08 11:15:02 +0300

    Start server

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const port = 3000;
 
 const app = express();
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
+app.use(express.json());
+
+app.post('/sign-up', (req, res) => {
+  res.send('Got a POST request');
 });
 
 // eslint-disable-next-line no-console

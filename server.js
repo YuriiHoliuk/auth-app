75ab2a77efa76b143f97fdabd6f91132ab3f7790
aleksandrDnieper <aleksandr.dnieper@gmail.com>
2020-04-08 11:11:46 +0300

    comments/id

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
+app.get('/comments/id', (req, res) => {
+  res.json('this page comments/id ');
 });
 
 // eslint-disable-next-line no-console

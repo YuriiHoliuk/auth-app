4c8c083f7bceaa248fbf2748ffa66e0646b2a5a1
Yurii Holiuk <bionicuss@gmail.com>
2020-04-08 10:41:29 +0300

    add get-users

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const express = require('express');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+app.get('/users', (req, res) => {
+  const users = [
+    {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe'
+    },
+    {
+      id: 2,
+      firstName: 'Brad',
+      lastName: 'Traversy'
+    },
+  ];
+
+  res.json(users);
+});
+
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+

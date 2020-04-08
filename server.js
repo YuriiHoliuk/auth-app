1a106eff4b1210b0816e0d0d281d3bd04b6d026d
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-04-08 11:24:36 +0300

    get-todos

--- server.js
+++ server.js
@@ -12,5 +12,29 @@ app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
+app.get('/todos', (req, res) => {
+  const todos = [
+    {
+      userId: 1,
+      id: 1,
+      title: 'delectus aut autem',
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: 'quis ut nam facilis et officia qui',
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 3,
+      title: 'fugiat veniam minus',
+      completed: false,
+    }];
+
+  res.json(todos);
+});
+
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server listening on port ${port}`));

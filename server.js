7c0bcdb6fb1ebdb1e80b00ab6040c45638274356
Arimazz <bodyaprymyak@icloud.com>
2020-04-08 11:08:20 +0300

    bohdan prymyak patch with id

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = 3000;
 
 const app = express();
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
+app.patch('/posts/:id', (req, res) => {
+  res.send(`Post with id:${req.params.id} is updated`);
 });
 
 // eslint-disable-next-line no-console
